baf55fd2596cf4ca0bf0c6cddae6f1ec9f511d8a
Crepper710 <thorlimberg@gmail.com>
2021-11-13 19:32:27 +0100

    Added first release with file for standard in/outputs

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar ScriptConverter.jar "Text Adventure script.txt" "12_05 08.11.2021.html.vxml" true
+java -jar ScriptConverter.jar "Text Adventure script.txt" "12_05 08.11.2021.html.min.vxml" false
