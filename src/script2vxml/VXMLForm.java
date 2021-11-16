c88120a8812cd28ed78fc7135a8afabf5ebebb1b
Crepper710 <thorlimberg@gmail.com>
2021-11-16 20:31:01 +0100

    Fixed field naming

--- src/script2vxml/VXMLForm.java
+++ src/script2vxml/VXMLForm.java
@@ -108,7 +108,7 @@ public class VXMLForm {
 	}
 	
 	public static String convertId(int id) {
-		return Integer.toString(id, 2).replace('0', 'l').replace('1', 'I');
+		return String.valueOf(id).replace('0', 'a').replace('1', 'b').replace('2', 'c').replace('3', 'd').replace('4', 'e').replace('5', 'f').replace('6', 'g').replace('7', 'h').replace('8', 'i').replace('9', 'j');
 	}
 	
 }
